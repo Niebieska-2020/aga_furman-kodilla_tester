3a4a2ebe6ca48e031774b4b920c4d164c56c3553
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-03-22 13:09:07 +0100

    Exercise 1, module 3.2 - Display the array elements in loop for

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,8 +1,11 @@
 public class Loops {
 
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
+
+        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
+
+         for (int i = 0; i < names.length; i++) {
+         System.out.println(names[i]);
         }
     }
 }
