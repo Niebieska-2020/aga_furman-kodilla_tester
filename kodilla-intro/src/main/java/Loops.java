366025526e712774240363d349a13db8aecfb441
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-03-22 13:18:59 +0100

    Exercise 1, module 3.2 - Display all items to the end

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -7,5 +7,9 @@ public class Loops {
          for (int i = 0; i < names.length; i++) {
          System.out.println(names[i]);
         }
+
+         for (int i = names.length - 1; i >= 0; i--) {
+             System.out.println(names[i]);
+         }
     }
 }
