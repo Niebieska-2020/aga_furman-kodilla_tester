dde6dc8867f8001f314176c1814ebe7f492926de
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-03-22 18:24:36 +0100

    Exercise 1, module 3.2 - Sum of elements in the table

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -7,9 +7,21 @@ public class Loops {
          for (int i = 0; i < names.length; i++) {
          System.out.println(names[i]);
         }
-
          for (int i = names.length - 1; i >= 0; i--) {
              System.out.println(names[i]);
          }
+    }
+    public int sumNumbers(int[] numbers) {
+
+        numbers[0] = 4;
+        numbers[1] = 7;
+        numbers[3] = 10;
+
+        int result = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            result = result + numbers[i];
+            }
+        return result;
     }
 }
