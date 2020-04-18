0c0be02e9cef0f71a1e970d07dd1354973bedfdd
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-04-18 23:57:25 +0200

    task.4.2.a draw cars

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
@@ -1,4 +1,28 @@
 package com.kodilla.collections.arrays.homework;
 
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Opel;
+import com.kodilla.collections.interfaces.homework.Toyota;
+
 public class CarUtils {
+    public static void describeCar(Car car) {
+        System.out.println("-------------------------");
+        System.out.println("Brand car");
+        System.out.println("Output speed: " + car.getSpeed());
+        System.out.println("Increased speed: " + car.increaseSpeed());
+        System.out.println("Decreased speed: " + car.decreaseSpeed());
+
+    }
+
+    private static String getCarName(Car car) {
+        if (car instanceof Ford)
+            return "Ford";
+        else if (car instanceof Opel)
+            return "Opel";
+        else if (car instanceof Toyota)
+            return "Toyota";
+        else
+            return "Unknown car brand.";
+    }
 }
--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
@@ -1,4 +1,32 @@
 package com.kodilla.collections.arrays.homework;
 
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Opel;
+import com.kodilla.collections.interfaces.homework.Toyota;
+
+import java.util.Random;
+
 public class CarsApplication {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Car[] cars = new Car[random.nextInt(15) +1];
+        for (int n = 0; n < cars.length; n++)
+            cars[n] = drawCar();
+       for (Car car : cars)
+           CarUtils.describeCar(car);
+    }
+
+    public static Car drawCar() {
+        Random random = new Random();
+        int drawBrandCar = random.nextInt(3);
+        int drawIncreaseSpeed = random.nextInt(110) + 10;
+        if (drawBrandCar == 0)
+            return new Ford(drawIncreaseSpeed);
+        else if (drawBrandCar == 1)
+                return new Opel(drawIncreaseSpeed);
+        else
+            return new Toyota(drawIncreaseSpeed);
+    }
 }
