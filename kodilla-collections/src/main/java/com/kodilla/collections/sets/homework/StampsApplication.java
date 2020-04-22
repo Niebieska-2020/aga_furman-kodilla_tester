76d336bdcb0b5e86eaefdf9e600c608564ec7cb5
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-04-23 00:07:48 +0200

    task.4.4 stamp

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -0,0 +1,21 @@
+package com.kodilla.collections.sets.homework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StampsApplication {
+    public static void main(String[] args) {
+        Set<Stamp> stamps = new HashSet<>();
+        stamps.add(new Stamp("Usual", "L", true));
+        stamps.add(new Stamp("Poland", "L", false));
+        stamps.add(new Stamp("Usual", "M", false));
+        stamps.add(new Stamp("Poland", "L", false));
+        stamps.add(new Stamp("Poland", "L", true));
+
+        System.out.println(stamps.size());
+
+        for (Stamp stamp: stamps) {
+            System.out.println(stamp);
+        }
+    }
+}
