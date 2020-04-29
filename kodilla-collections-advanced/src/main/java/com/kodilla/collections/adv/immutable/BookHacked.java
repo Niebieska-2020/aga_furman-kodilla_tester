720dd9b96d94779509a0ec59f19d8aa116da52ea
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-04-29 21:55:05 +0200

    example.5.3 immutability update

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/BookHacked.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/BookHacked.java
@@ -1,4 +1,19 @@
 package com.kodilla.collections.adv.immutable;
 
-public class BookHacked {
+public class BookHacked extends Book {
+    private  String realTitle;
+
+    public BookHacked(String author, String title) {
+        super(author, title);
+        realTitle = title;
+    }
+
+    public void modifyTitle(String newTitle) {
+        realTitle = newTitle;
+    }
+
+    @Override
+    public String getTitle() {
+        return realTitle;
+    }
 }
