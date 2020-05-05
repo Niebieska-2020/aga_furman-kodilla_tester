01a923c8e6221c61fbda402cc7898a48b1b8b7c8
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-05-05 19:58:49 +0200

    task.5.5 refactoring

--- kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuit.java
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuit.java
@@ -1,6 +1,5 @@
 package com.kodilla.collections.adv.exercises.homework;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -14,43 +13,27 @@ public class FlightFinderTestSuit {
     public void findFlightsFrom() {
         // given
         FlightFinder flightFinder = new FlightFinder(FlightRepository.getFlightsTable());
-        List<Flight> resultFlightFrom = flightFinder.findFlightsFrom("Warszawa");
-
-        // when
         List<Flight> expectedFlightsList = new ArrayList<>();
         expectedFlightsList.add(new Flight("Warszawa", "Krakow"));
         expectedFlightsList.add(new Flight("Warszawa", "Gdansk"));
+
+        // when
+        List<Flight> resultFlightFrom = flightFinder.findFlightsFrom("Warszawa");
 
         // then
         assertEquals(expectedFlightsList, resultFlightFrom);
     }
 
     @Test
-    public void testFindFlightFromWarszawa() {
+    public void testFindFlightFromWhenWeHaveNotThisCity () {
         // given
         FlightFinder testFlightFinder = new FlightFinder(FlightRepository.getFlightsTable());
 
         // when
-        List<Flight> foundFlightFrom = testFlightFinder.findFlightsFrom("Warszawa");
+        List<Flight> weHaveNotThisCity = testFlightFinder.findFlightsFrom("Poznań");
 
         // then
-        for (Flight flight : foundFlightFrom) {
-            assertEquals("Warszawa", flight.getDeparture());
-        }
-    }
-
-    @Test
-    public void testFindFlightToWhenDepartureIsWrongWritten() {
-        // given
-        FlightFinder testFlightFinder = new FlightFinder(FlightRepository.getFlightsTable());
-
-        // when
-        List<Flight> foundFlightFrom = testFlightFinder.findFlightsFrom("Warsszawa");
-
-        // then
-        for (Flight flight : foundFlightFrom) {
-            Assertions.assertNotSame("Warsszawa", flight.getDeparture());
-        }
+        assertNotSame(weHaveNotThisCity, testFlightFinder);
     }
 
     @Test
@@ -62,9 +45,7 @@ public class FlightFinderTestSuit {
         List<Flight> notFoundFlightFrom = testFlightFinder.findFlightsFrom(" ");
 
         // then
-        for (Flight flight : notFoundFlightFrom) {
-            assertEquals(" ", flight.getDeparture());
-        }
+        assertNotEquals(notFoundFlightFrom, testFlightFinder);
     }
 
     @Test
@@ -76,52 +57,34 @@ public class FlightFinderTestSuit {
         List<Flight> notFoundFlightFrom = testFlightFinder.findFlightsFrom("null");
 
         // then
-        for (Flight flight : notFoundFlightFrom) {
-            assertNull(notFoundFlightFrom, flight.getDeparture());
-        }
+        assertNotEquals(notFoundFlightFrom, testFlightFinder);
     }
 
     @Test
     public void findFlightsTo() {
         // given
         FlightFinder testFlightFinder = new FlightFinder(FlightRepository.getFlightsTable());
-        List<Flight> resultFlightTo = testFlightFinder.findFlightsTo("Krakow");
-
-        // when
         List<Flight> expectedFlightsList = new ArrayList<>();
         expectedFlightsList.add(new Flight("Warszawa", "Krakow"));
         expectedFlightsList.add(new Flight("Gdansk", "Krakow"));
+
+        // when
+        List<Flight> resultFlightTo = testFlightFinder.findFlightsTo("Krakow");
 
         // then
         assertEquals(expectedFlightsList, resultFlightTo);
     }
 
     @Test
-    public void testFindFlightToWarszawa() {
+    public void testFindFlightToWhenWeHaveNotThisCity() {
         // given
         FlightFinder testFlightFinder = new FlightFinder(FlightRepository.getFlightsTable());
 
         // when
-        List<Flight> foundFlightTo = testFlightFinder.findFlightsTo("Warszawa");
+        List<Flight> weHaveNotThisCity = testFlightFinder.findFlightsTo("Poznań");
 
         // then
-        for (Flight flight : foundFlightTo) {
-            assertEquals("Warszawa", flight.getArrival());
-        }
-    }
-
-    @Test
-    public void testFindFlightToWhenArrivalIsWrongWritten() {
-        // given
-        FlightFinder testFlightFinder = new FlightFinder(FlightRepository.getFlightsTable());
-
-        // when
-        List<Flight> foundFlightTo = testFlightFinder.findFlightsTo("Warsszawa");
-
-        // then
-        for (Flight flight : foundFlightTo) {
-            assertNotSame("Warsszawa", flight.getArrival());
-        }
+        assertNotSame(weHaveNotThisCity, testFlightFinder);
     }
 
     @Test
@@ -133,9 +96,7 @@ public class FlightFinderTestSuit {
         List<Flight> notFoundFlightTo = testFlightFinder.findFlightsTo(" ");
 
         // then
-        for (Flight flight : notFoundFlightTo) {
-            assertEquals(" ", flight.getArrival());
-        }
+        assertNotEquals(notFoundFlightTo, testFlightFinder);
     }
 
     @Test
@@ -147,8 +108,6 @@ public class FlightFinderTestSuit {
         List<Flight> notFoundFlightTo = testFlightFinder.findFlightsTo("null");
 
         // then
-        for (Flight flight : notFoundFlightTo) {
-            assertNotEquals(notFoundFlightTo, flight.getArrival());
-        }
+        assertNotEquals(notFoundFlightTo, testFlightFinder);
     }
 }
