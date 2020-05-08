911c2985bbdf9706fe978eb8fd8b76222066b6f2
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-05-08 16:53:35 +0200

    task.6.3 refactoring

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -7,18 +7,18 @@ public class ForumStats {
     public static void main(String[] args) {
         double averageNumberOfPostsWhenUserAgeIsUp40 = UsersRepository.getUsersList()
             .stream()
-            .filter(a -> a.getAge() >= 40 && a.getNumberOfPosts() !=0)
+            .filter(a -> a.getAge() >= 40)
             .mapToInt(User::getNumberOfPosts)
             .average()
-            .getAsDouble();
+            .orElse(0);
         System.out.println("Average number of posts users which age is more than 40: " + averageNumberOfPostsWhenUserAgeIsUp40);
 
         double averageNumberOfPostsWhenUserAgeIsBelow40 = UsersRepository.getUsersList()
             .stream()
-            .filter(a -> a.getAge() < 40 && a.getNumberOfPosts() !=0)
+            .filter(a -> a.getAge() < 40)
             .mapToInt(User::getNumberOfPosts)
             .average()
-            .getAsDouble();
+            .orElse(0);
         System.out.println("Average number of posts users which age is less than 40: " + averageNumberOfPostsWhenUserAgeIsBelow40);
     }
 }
