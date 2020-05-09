2e9d5e0cbe1af44adba5d69730e7632e50740cec
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-05-09 14:30:13 +0200

    task.6.5 improved test method when the order does not exist

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -2,39 +2,31 @@ package com.kodilla.exception.homework;
 
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.junit.Assert.*;
 
 public class WarehouseTestSuite {
 
+    Warehouse warehouse = new Warehouse();
+
     @Test
     public void testGetOrderExists() throws OrderDoesntExistException {
         // given
-        Warehouse warehouse = new Warehouse();
         warehouse.addOrder(new Order("5/5/2020"));
 
         // when
         Order result = warehouse.getOrder("5/5/2020");
 
         // then
-        assertEquals(new Order("5/5/2020"), result);
+        assertEquals("5/5/2020", result.getNumber());
     }
 
     @Test
-    public void testGetOrderDoseNotExists() throws OrderDoesntExistException {
-        // given
-        Warehouse warehouse = new Warehouse();
-        Set<Order> expectedOrder = new HashSet<>();
-        expectedOrder.add(new Order("1/5/2020"));
-        expectedOrder.add(new Order("2/5/2020"));
-        expectedOrder.add(new Order("3/5/2020"));
-
-        // when
-        Order result = warehouse.getOrder("5/05/2020");
-
-        // then
-        assertNotEquals(expectedOrder, result);
+    public void testGetOrderDoseNotExists() {
+        try {
+            Order result = warehouse.getOrder("5/05/2020");
+            return;
+        } catch (Exception e) {
+            System.out.println("This order does not exist");
+        }
     }
 }
