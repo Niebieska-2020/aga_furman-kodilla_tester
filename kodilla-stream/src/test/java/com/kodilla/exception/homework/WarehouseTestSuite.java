1a818da52d1f512bd0f614464162fa0040fda365
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-05-10 21:36:41 +0200

    test.6.5 refactoring testGetOrderDoseNotExists

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class WarehouseTestSuite {
 
-    Warehouse warehouse = new Warehouse();
+    private Warehouse warehouse = new Warehouse();
 
     @Test
     public void testGetOrderExists() throws OrderDoesntExistException {
@@ -20,13 +20,11 @@ public class WarehouseTestSuite {
         assertEquals("5/5/2020", result.getNumber());
     }
 
-    @Test
-    public void testGetOrderDoseNotExists() {
-        try {
-            Order result = warehouse.getOrder("5/05/2020");
-            return;
-        } catch (Exception e) {
-            System.out.println("This order does not exist");
-        }
+    @Test(expected = OrderDoesntExistException.class)
+    public void testGetOrderDoseNotExists() throws OrderDoesntExistException {
+        // given
+        warehouse.addOrder(new Order("3/5/2020"));
+        // when
+        warehouse.getOrder("5/05/2020");
     }
 }
