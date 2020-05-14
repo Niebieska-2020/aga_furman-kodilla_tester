61ad74059f21fa6f168aec8febf7c6d791497c07
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-05-14 19:22:41 +0200

    task.7.1 refactoring

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/UserDoesntExist.java
+++
@@ -1,4 +0,0 @@
-package com.kodilla.parametrized_tests.homework;
-
-public class UserDoesntExist extends Exception {
-}
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -1,56 +1,70 @@
 package com.kodilla.parametrized_tests.homework;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserValidatorTestSuite {
 
     private UserValidator userValidator = new UserValidator();
 
     @ParameterizedTest
-    @ValueSource(strings = {"ach.TO_b2B", "Java-tester123", "00__00", "TTT.-.WWW"})
-    public void shouldReturnTrueForValidateUsernameIsMoreThenThreeCharacters(String username) throws UserDoesntExist {
+    @ValueSource(strings = {"ach.TO_b2B", "TTT.-.WWW00__00"})
+    public void shouldReturnTrueForValidateUsernameIsMoreThenThreeCharacters(String username) {
         boolean result = userValidator.validateUsername(username);
         assertTrue(result);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"!@#$%^&*()_+", "{[;:]}", "/|,<>?", "Zażółć", "gęślą", "jaźń"})
-    public void shouldReturnFalseForValidateUserNameIfItUsesSpecialOrPolishCharacters(String username) throws UserDoesntExist {
+    @ValueSource(strings = {"!@#$%^&*()_+/|,<>?{[;:]}", "Zażółć gęślą jaźń"})
+    public void shouldReturnFalseForValidateUserNameIfItUsesSpecialOrPolishCharacters(String username) {
         assertFalse(userValidator.validateUsername(username));
     }
 
     @ParameterizedTest
-    @NullAndEmptySource
-    public void shouldReturnFalseIfValidateUserNameIsEmpty(String username) throws UserDoesntExist {
+    @ValueSource(strings = {"X", ".."})
+    public void shouldReturnFalseIfValidateUserNameHasLessThenThreeCharacters(String username) {
         assertFalse(userValidator.validateUsername(username));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"X", "QQ", "v", "yy", ".", "..", "_", "__", "-", "--"})
-    public void shouldReturnFalseIfValidateUserNameHasLessThenThreeCharacters(String username) throws UserDoesntExist {
+    @EmptySource
+    public void shouldReturnFalseWhenUsernameIsEmpty(String username) {
         assertFalse(userValidator.validateUsername(username));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    public void shouldThrowExceptionWhenUsernameIsNull(String username) {
+        assertThrows(NullPointerException.class, () -> userValidator.validateUsername(username));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"ale@onet.pl", "234@wp.pl", "W.1-@3a.pl", "_@34.pl"})
-    public void shouldReturnTrueIfValidateEmailUsedCorrectValue(String email) throws UserDoesntExist {
+    public void shouldReturnTrueIfValidateEmailUsedCorrectValue(String email) {
         assertTrue(userValidator.validateEmail(email));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"abra.good", "abra@good", "2304@05958", "2304.05958", ".entery", "@entery", "@@@", "2@2", "END@END", "END", "@END", "Zażółć@r.pl"})
-    public void shouldReturnFalseIfValidateEmailUsedWrongValue(String email) throws UserDoesntExist {
+    @ValueSource(strings = {" ", "abra.good", "abra7@good5", ".entery", "@entery", "@@@", "2@2", "Zażółć@r.pl"})
+    public void shouldReturnFalseIfValidateEmailUsedWrongValue(String email) {
         assertFalse(userValidator.validateEmail(email));
     }
 
     @ParameterizedTest
-    @NullAndEmptySource
-    public void shouldReturnFalseIfValidateEmailIsEmpty(String email) throws UserDoesntExist {
+    @NullSource
+    public void shouldThrowExceptionWhenEmailIsNull(String email) {
         assertFalse(userValidator.validateEmail(email));
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    public void shouldReturnFalseWhenEmailIsEmpty(String email) {
+        assertTrue(userValidator.validateEmail(email));
     }
 }
