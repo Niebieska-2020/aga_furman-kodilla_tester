9ff59607aa61a95a867a24b3c9f95fa1e271cd1b
Niebieska-2020 <62118079+Niebieska-2020@users.noreply.github.com>
2020-06-06 13:45:26 +0200

    task.7.2 testing gambling machine (#40)
    * task.7.2 preparation
    
    * taks.7.2 refactoring

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -0,0 +1,50 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class GamblingMachineTestSuite {
+
+    private final GamblingMachine testee = new GamblingMachine();
+
+    @Test
+    public void shouldReturnExceptionWhenUserNumbersIsEmpty() {
+        assertThrows(InvalidNumbersException.class, () -> testee.howManyWins(new HashSet<>()));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/correctSetOfNumbers.csv")
+    public void shouldReturnExceptionWhenSetOfNumbersIsCorrectForUserNumber(String numbers) throws InvalidNumbersException {
+        // Given
+        String[] split = numbers.split(" ");
+        Set<Integer> integers = new HashSet<>();
+        for (String string : split) {
+            integers.add(Integer.valueOf(string));
+        }
+
+        // When Then
+        testee.howManyWins(integers);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/uncorrectSetOfNumbers.csv")
+    public void shouldReturnExceptionWhenSetOfNumbersIsUncorrectForUserNumber(String userNumbers) throws InvalidNumbersException {
+        // Given
+        String[] split = userNumbers.split(" ");
+        Set<Integer> integers = new HashSet<>();
+        for (String string : split) {
+            integers.add(Integer.valueOf(string));
+        }
+
+        // When Then
+        assertThrows(InvalidNumbersException.class, () -> {
+            testee.howManyWins(integers);
+        });
+    }
+}
