53add2106c142e5f93a081022fe62ece4f785500
Agnieszka Furman <agnieszka.furman7@gmail.com>
2020-06-24 16:37:21 +0200

    module.13.3 preparation

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -0,0 +1,12 @@
+package com.kodilla.selenium.google;
+
+import org.openqa.selenium.WebDriver;
+import
+
+public class GoogleExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+    }
+}
